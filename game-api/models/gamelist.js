1c5c986c7385c56b4cc57513e9dc3e6a3db58a9a
hoangkimminh <vatliyeutoi@gmail.com>
2019-05-07 13:42:21 +0700

    fix contact controller

--- game-api/models/gamelist.js
+++ game-api/models/gamelist.js
@@ -6,13 +6,5 @@ exports.gameList = [
     description:
       "Classic là chế độ chơi cơ bản. Vượt qua 10 câu hỏi với chủ để ngẫu nhiên và giành số điểm cao nhất.",
     id: "classic"
-  },
-  {
-    name: "Topic: Chủ đề",
-    imageLink: "https://pbs.twimg.com/media/DeTDi4TV4AEyEtq.jpg",
-    questionAmount: 10,
-    description:
-      "Topic là chế độ chơi với chủ đề tuỳ chọn. Vượt qua 10 câu hỏi và giành số điểm cao nhất.",
-    id: "topic"
   }
 ];
