84b8cc884bc07a55b2f37f55c15bb1beaa364afd
thanhvh99 <vuhathanh00@gmail.com>
2019-05-11 13:05:02 +0700

    Update

--- game-api/models/gamelist.js
+++ game-api/models/gamelist.js
@@ -4,7 +4,7 @@ exports.gameList = [
     imageLink: "http://www.accesa.com.sv/wp-content/uploads/2017/09/question.png",
     questionAmount: 10,
     description:
-      "Classic là chế độ chơi cơ bản. Vượt qua 10 câu hỏi với chủ để ngẫu nhiên và giành số điểm cao nhất.",
+      "Classic là chế độ chơi cơ bản. Vượt qua 10 câu hỏi với chủ đề ngẫu nhiên và giành số điểm cao nhất.",
     id: "classic"
   }
 ];
