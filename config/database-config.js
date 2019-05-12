fb151028d55e66b403a34310a29409f884518eff
hoangkimminh <vatliyeutoi@gmail.com>
2019-05-12 10:30:39 +0700

    Update database-config.js

--- config/database-config.js
+++ config/database-config.js
@@ -3,15 +3,15 @@ var env = process.env.NODE_ENV || "development";
 
 module.exports = {
   "development" : {
-    "username": process.env.USER ||"zQfQZVB6qJ",
-    "password":  process.env.PASSWORD || "pJqvhIvRPA",
-    "database": process.env.DB || "zQfQZVB6qJ",
-    "host": process.env.DBHOST || "remotemysql.com",
+    "username": process.env.USER ||"root",
+    "password":  process.env.PASSWORD || "",
+    "database": process.env.DB || "my_schema",
+    "host": process.env.DBHOST || "localhost",
     "dialect": "mysql",
     "connection": {
-      "host": process.env.DBHOST || "remotemysql.com",
-      "user": process.env.USER ||"zQfQZVB6qJ",
-      "password": process.env.PASSWORD || "pJqvhIvRPA"
+      "host": process.env.DBHOST || "localhost",
+      "user": process.env.USER ||"root",
+      "password": process.env.PASSWORD || ""
     }
   }
   
