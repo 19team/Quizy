72117cdb5a77cd8e96ebf1e624eb58935839af73
hoangkimminh <vatliyeutoi@gmail.com>
2019-05-12 14:32:38 +0700

    Sửa code xấu

--- models/index.js
+++ models/index.js
@@ -2,9 +2,7 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var config = require(
-  "../config/database-config"
-);
+var config = require("../config/database-config");
 var sequelize = new Sequelize(
   config.database,
   config.username,
@@ -13,15 +11,15 @@ var sequelize = new Sequelize(
 );
 var db = {};
 fs.readdirSync(__dirname)
-  .filter(function (file) {
+  .filter(function(file) {
     return file.indexOf(".") !== 0 && file !== "index.js";
   })
-  .forEach(function (file) {
+  .forEach(function(file) {
     var model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function (modelName) {
+Object.keys(db).forEach(function(modelName) {
   if ("associate" in db[modelName]) {
     db[modelName].associate(db);
   }
